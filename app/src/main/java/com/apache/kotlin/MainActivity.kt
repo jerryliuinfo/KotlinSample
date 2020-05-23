feae01bee62bfe838e8e7fec3f939f92294e3226
liuxiangxiang1234@163.com <gree3502>
2020-05-23 15:59:57 +0800

    [内容] lambda表达式

--- app/src/main/java/com/apache/kotlin/MainActivity.kt
+++ app/src/main/java/com/apache/kotlin/MainActivity.kt
@@ -1,10 +1,42 @@
 package com.apache.kotlin
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 
 open  class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    lateinit var textView:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        textView.setOnClickListener(fun (v: View):Unit{
+
+        })
+
+        textView.setOnClickListener({ v: View ->
+
+        })
+
+        //如果lambda 是函数的最后一个参数，可以把lambda写在括号的外面，变成
+        textView.setOnClickListener(){ v: View ->
+
+        }
+
+        //如果lamada 是 函数唯一的参数，可以直接把括号去了，变成
+        textView.setOnClickListener{ v: View ->
+
+        }
+
+        //Kotlin对于唯一的参数有默认的名字:it
+        textView.setOnClickListener{
+        }
+    }
 
 
 
